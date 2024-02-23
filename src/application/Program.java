cbf810d03bd8964978089a93d768a108e45e7e20
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-23 07:11:06 -0300

    lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,8 +17,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        double min = 100.0;
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
 
         for (Product p : list) {
             System.out.println(p);
