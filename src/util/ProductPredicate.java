9ec4ca421f9151adb775244c1b70345735198c97
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-23 06:39:30 -0300

    Initial commit

---
+++ src/util/ProductPredicate.java
@@ -0,0 +1,17 @@
+package util;
+
+import entitites.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+
+    @Override
+    public boolean test(Product p) {
+        return p.getPrice() >= 100.0;
+    }
+
+
+
+}
